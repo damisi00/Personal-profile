33723df35d54b96dccd5e91b6590e494ee61e7da
[damisi00] <enioluwafeo@gmail.com>
2022-11-05 22:38:23 +0100

    feature: form validation

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,42 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+// name
 const name = 'Oluwadamisi Enioluwafe';
 
+
 const ContactPage = () => {
+    const [enteredMsg, setMsg] = useState('');
+    const [submit, setSubmit] = useState(false);
+    const [errorMsg, setError] = useState(false);
+
+    const handleSubmit = ev => {
+        
+        ev.preventDefault();
+        setSubmit(true);
+        if (enteredMsg.trim().length === 0) {
+             setError(true) 
+             return null;
+        };
+        setTimeout(() => {
+            setSubmit(false);
+        }, 4000);
+        
+    }
   return (
     <>
         <section className='contact-wrap'>
             <h1>Contact Me</h1>
             <p className='description'>Hi there, contact me to ask me about anything you have in mind.</p>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='grp'>
                     <div className='l-input'>
-                        <label for='first_name'>First name</label>
-                        <input id='first_name'  placeholder='Enter your first name' type='text'/>
+                        <label htmlFor='first_name'>First name</label>
+                        <input id='first_name'  placeholder='Enter your first name' type='text' required/>
                     </div>
                     
                     <div className='l-input'>
-                        <label for='last_name'>Last name</label>
-                        <input id='last_name'  placeholder='Enter your last name' type='text'/>
+                        <label htmlFor='last_name'>Last name</label>
+                        <input id='last_name'  placeholder='Enter your last name' type='text' required/>
                     </div>
                 </div>
                 <div className='grp '>
                     <div className='l-input ii'>
-                        <label for='email'>Email</label>
-                        <input id='email'  placeholder='yourname@email.com' type='email'/>
+                        <label htmlFor='email'>Email</label>
+                        <input id='email'  placeholder='yourname@email.com' type='email' required/>
                     </div>
                 </div>
                 <div className='grp'>
                     <div className='l-input ii'>
-                        <label for=''>Message</label>
-                        <textarea id=''  placeholder="Send me a message and I'll reply you as soon as possible..." cols='' rows='' />
+                        <label htmlFor='message'>Message</label>
+                        <textarea id='message' value={enteredMsg} 
+                            onChange={ev => setMsg(ev.target.value)}
+                            placeholder="Send me a message and I'll reply you as soon as possible..."  />
+                        {errorMsg && <h6 className='err-msg'>Please enter a message</h6>}
                     </div>
                 </div>
                 <div className='check'>
-                    <input type='checkbox' value='' id='' />
-                    <label for=''>You agree to providing your data to {name} who may contact you.</label>
+                    <input type='checkbox'  id='check_box' required/>
+                    <label htmlFor='check_box'>You agree to providing your data to {name} who may contact you.</label>
                 </div>
-                <button type='submit' id='btn__submit' >Send message</button>       
+                <button type='submit' id='btn__submit' >Send message</button>   
+                {submit && !errorMsg &&
+                    <p className="">Message sent successfully.</p>
+                }    
             </form>
         </section>
     </>
